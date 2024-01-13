3e88a944d9fa1cba289275fa6dfc5c7fb61d39be
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-13 12:22:39 +0530

    Add files via upload

---
+++ scope_two.py
@@ -0,0 +1,9 @@
+from scope_one import *
+class Beasanth_ecity(Besanth):
+    def show (self):
+        print(self._y)
+obj = Beasanth_ecity()
+obj.greet()
+obj.show_beasanth()
+obj.show()
+print(pqr)
